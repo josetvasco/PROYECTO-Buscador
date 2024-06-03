7847070ecfde54fe8af392fb4d844dbc5369012b
josetvasco <torovascojosedaniel@gmail.com>
2024-06-03 16:51:10 -0500

    Terminada las funciones de filtrados de cada de los selects con su muestra en el HTML en tiempo real

--- js/app.js
+++ js/app.js
@@ -37,36 +37,46 @@ year.addEventListener( 'change', (e) => {
 
 minimo.addEventListener( 'change', (e) => {
   datosBusqueda.minimo = e.target.value;
+  
+  filtrarAuto();
 })
 
 maximo.addEventListener( 'change', (e) => {
   datosBusqueda.maximo = e.target.value;
+
+  filtrarAuto();
 })
 
 puertas.addEventListener( 'change', (e) => {
-  datosBusqueda.puertas = e.target.value;
+  datosBusqueda.puertas = parseInt(e.target.value);
+
+  filtrarAuto();
 })
 
 transmision.addEventListener( 'change', (e) => {
   datosBusqueda.transmision = e.target.value;
+
+  filtrarAuto();
 })
 
 color.addEventListener( 'change', (e) => {
   datosBusqueda.color = e.target.value;
+
+  filtrarAuto();
 })
 
 // Eventos
 document.addEventListener( 'DOMContentLoaded', () => {
-  mostrarAutos(); // Muestra los automoviles al cargar
+  mostrarAutos(autos); // Muestra los automoviles al cargar
 
   // Llena las opciones de los años
   llenarSelect();
 });
 
 // Funciones
-function mostrarAutos() {
+function mostrarAutos(autos) {
+  limpiarHTML(); // Elimina el HTML previo
   autos.forEach( auto => {
-
     const { marca, modelo, year, puertas, transmision, precio, color } = auto;
     const autoHTML = document.createElement('P');
     autoHTML.textContent = `
@@ -74,6 +84,13 @@ function mostrarAutos() {
     `
     resultado.appendChild(autoHTML);
   })
+}
+
+// Limpia el HTML
+function limpiarHTML() {
+  while( resultado.firstChild ) {
+    resultado.removeChild( resultado.firstChild );
+  }
 }
 
 // Genera los años del select
@@ -89,9 +106,8 @@ function llenarSelect() {
 
 // Función que busca en base a la busqueda
 function filtrarAuto() {
-  const resultado = autos.filter( filtrarMarca ).filter( filtrarYear );
-
-  console.log(resultado)
+  const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
+  mostrarAutos(resultado);
 }
 
 function filtrarMarca(auto) {
@@ -104,6 +120,41 @@ function filtrarMarca(auto) {
 function filtrarYear(auto) {
   if( datosBusqueda.year ) {
     return auto.year === datosBusqueda.year;
+  }
+  return auto;
+}
+
+function filtrarMinimo(auto) {
+  if( datosBusqueda.minimo ) {
+    return auto.precio >= datosBusqueda.minimo;
+  }
+  return auto;
+}
+
+function filtrarMaximo(auto) {
+  if( datosBusqueda.maximo ) {
+    return auto.precio <= datosBusqueda.maximo;
+  }
+  return auto;
+} 
+
+function filtrarPuertas(auto) {
+  if( datosBusqueda.puertas ) {
+    return auto.puertas === datosBusqueda.puertas;
+  }
+  return auto;
+}
+
+function filtrarTransmision(auto) {
+  if( datosBusqueda.transmision ) {
+    return auto.transmision === datosBusqueda.transmision;
+  }
+  return auto;
+}
+
+function filtrarColor(auto) {
+  if( datosBusqueda.color ) {
+    return auto.color === datosBusqueda.color;
   }
   return auto;
 }
